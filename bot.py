23a10850a0f73721507204e356dc09626989b722
arshia2oo9 <arshiahallaji1388@gmail.com>
2026-06-09 22:34:33 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "سلام! من اولین بات تلگرامی تو هستم 🤖🚗"
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+app.run_polling()
